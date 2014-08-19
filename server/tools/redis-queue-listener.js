8471ec4545b9c8c501a876f602f77156addce325
Sam Stonehouse <sstonehouse@scottlogic.com>
2014-08-19 10:35:11 +0100

    Made redis monitor also display channel name

--- server/tools/redis-queue-listener.js
+++ server/tools/redis-queue-listener.js
@@ -3,7 +3,7 @@ var redis = require('redis');
 var completeSubscriber = redis.createClient();
 
 completeSubscriber.on('pmessage', function(channelPattern, actualChannel, message) {
-	console.log("[DEBUG][Subscriber]" + message);
+	console.log("[DEBUG][Subscriber][" + actualChannel + "]" + message);
 });
 
 completeSubscriber.psubscribe("*");
